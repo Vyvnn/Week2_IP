68e8a7de2952e481d0d9d32a283e3483192f4ac3
Vivian <vivyan3gmail.com>
2022-09-28 18:25:59 +0300

    buttons added,styling added,landing page built

---
+++ js/main.js
@@ -0,0 +1,12 @@
+const MALE_NAMES =["Kwasi"];
+const FEMALE_NAMES =["Akosua"];
+const DAY_OF_WEEK =["sunday"];
+var day = null;
+var month = null;
+
+//get user input
+    //get user's birthday,
+    //get user's gender m,f
+
+//calculate day of birth
+//get user's akhan name
