820bef6fd33e115520206ccc95307e87ad3ac2c4
Vivian <vivyan3gmail.com>
2022-09-29 09:31:27 +0300

    select style added

--- js/main.js
+++ js/main.js
@@ -14,5 +14,32 @@ var month = null;
 //get user's akhan name
 
 function getUserInput(){
-
+day = prompt("Enter your day:");
+month=prompt("Enter your month:");
+year=prompt("Enter your year:");
+gender=prompt("Enter your gender:");
+century=prompt("Enter your century:");year,slice(0,2);
 }
+function getAkhanNames(day,month,year,gender){
+    var dayOfTheWeek =
+    var indexOfDay = DAY_OF_WEEK.indexOf(dayOfTheWeek);
+    var name= null;
+    if (gender.toLocaleLowercase().startswith('m')){
+        name= MALE_NAMES[indexOfDay];
+    }else if (gender.toLocaleLowercase().startswith('f')){
+        name = FEMALE_NAMES[indexOfDay]
+    }else {
+        console.log ("Invalid gender!")
+    }
+    return name;
+}
+function main(){
+    getUserInput{};
+    
+}
+
+
+
+
+
+//document.getElementById ("Your Akhan name is").innerText= Kwasi
